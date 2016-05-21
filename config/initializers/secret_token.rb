976499f60d8c975137c25555cf0f90978b6c8f74
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-05-21 19:12:46 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grauer::Application.config.secret_key_base = '0429b8ffc2c00313b08efa9fd04980fc772b3dc840ba9a11cb7bd9cb51395f3d8cf6677a04b9535af530cca3327966c74c8ad167bf561bf569cf6dfa7b021382'
