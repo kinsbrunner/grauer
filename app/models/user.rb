72cbf8541124bc262f53045878702294978eca9f
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-05-23 22:42:36 +0000

    Additional styling to Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,4 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :authentication_keys => [:username]
 
-=begin     
-  def email_required?
-    false
-  end  
-
-  def email_changed?
-    false
-  end 
-=end
 end
