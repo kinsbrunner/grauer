355591c1d653503b63f11f7a486568743d97488b
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-05-23 00:49:23 +0000

    Adding additional fields to Users model

---
+++ db/migrate/20160523001446_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    
+    add_index :users, :username, unique: true
+  end
+end
