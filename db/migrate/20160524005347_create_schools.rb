a4e106e1e454be17398892f3b4136b3fd8095b75
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-05-24 01:02:46 +0000

    Added School model

---
+++ db/migrate/20160524005347_create_schools.rb
@@ -0,0 +1,9 @@
+class CreateSchools < ActiveRecord::Migration
+  def change
+    create_table :schools do |t|
+      t.string :name
+      t.timestamps
+    end
+    add_index :schools, :name, unique: true
+  end
+end
