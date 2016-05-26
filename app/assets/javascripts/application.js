a9b026e7bf4c2ec95d659e945b21784154de7837
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-05-26 03:50:57 +0000

    Added responsivness to DataTable

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,5 +13,6 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require dataTables/jquery.dataTables
+//= require dataTables/extras/dataTables.responsive
 //= require turbolinks
 //= require_tree .
