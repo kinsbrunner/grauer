ad59c56d7280788f569ada919af2f2f1f9d49e29
Alejandro Kinsbrunner <kinsbrunner@gmail.com>
2016-06-06 01:50:59 +0000

    Fixing Delete child

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require dataTables/jquery.dataTables
 //= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
