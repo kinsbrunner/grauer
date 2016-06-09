823b8e9dd5bacf768dda01722fffd13e87a51156
kinsbrunner <kinsbrunner@hotmail.com>
2016-06-09 15:00:29 -0400

    Added Foods controller and index, new, create and destroy logic

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,38 @@
+class FoodsController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :new, :create, :destroy]
+  
+  def index
+    @foods = Food.order(:comida).all
+  end
+  
+  def new
+    @food = Food.new
+  end
+  
+  def create
+    @food = current_food.create(food_params)
+    if @food.valid?
+      redirect_to foods_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  def destroy
+    return render_not_found if current_food.blank?
+    current_food.destroy
+    redirect_to foods_path
+  end
+  
+    
+  private
+  
+  def food_params
+      params.require(:food).permit(:tipo, :comida, :siempre)
+  end
+  
+  helper_method :current_food
+  def current_food
+    @current_food ||= Food.find_by_id(params[:id])
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160609182708) do
+ActiveRecord::Schema.define(version: 20160609185512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,7 @@ ActiveRecord::Schema.define(version: 20160609182708) do
 
   create_table "foods", force: true do |t|
     t.integer  "tipo"
-    t.string   "nota"
+    t.string   "comida"
     t.boolean  "siempre"
     t.datetime "created_at"
     t.datetime "updated_at"
