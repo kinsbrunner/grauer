132c46a0c0beea3a18d1573e06771af882786acd
kinsbrunner <kinsbrunner@hotmail.com>
2016-06-21 16:26:44 -0400

    Retrieving Platos de comida

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,3 +1,8 @@
 class MenusController < ApplicationController
   before_action :authenticate_user!, only: [:index]
+  
+  def index
+    @foods_p = Food.where("tipo = "+ Food::TIPO_COMIDAS['Principal'].to_s).order(:comida)
+    @foods_a = Food.where("tipo = "+ Food::TIPO_COMIDAS['Acompañamiento'].to_s).order(:comida)
+  end
 end
