ed2afdb1730afec0516bbd14448cf8bb507f1614
kinsbrunner <kinsbrunner@hotmail.com>
2016-07-11 17:05:20 -0400

    Hidding from Deudores list those who do not owe money

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,28 +8,33 @@ class ListsController < ApplicationController
   def show
     @reporte_id = params[:id]
     if @reporte_id == '1'
+      
       @families = current_school.families.order(:apellido).page(params[:page])
+      
     elsif @reporte_id == '2'
+      
       @schools = School.order(:name)
       @months = 1..12
       @incomes = Movement.select("extract(year from created_at) as anio, extract(month from created_at) as mes, school_id, sum(monto) as income").where("tipo = 1 AND extract(year from created_at) = date_part('year', CURRENT_DATE)").group("anio, mes, school_id")  
-      
       @ingresos = Hash.new
       @schools.each do |s|
         @ingresos[s.name] = Hash.new
         @months.each do |mes|
           @ingresos[s.name][mes] = 0
         end
-      end      
-      
+      end
       @incomes.each do |inc|
         @ingresos[inc.school.name][inc.mes.to_i] = inc.income
       end
       
     elsif @reporte_id == '3'
+    
       @students = current_school.children.page(params[:page])
+    
     else
+      
       return render_not_found
+    
     end
   end
   
