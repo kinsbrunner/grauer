e7a88318c91dbae248da392751329952e4192a90
kinsbrunner <kinsbrunner@hotmail.com>
2016-07-14 17:13:41 -0400

    Added new fonts to PDF and started working over ribbon

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,10 @@ module Grauer
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :es
     config.i18n.available_locales = :es
+    
+    # Add the fonts path
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
+    # Precompile additional assets
+    config.assets.precompile += %w( .svg .eot .woff .ttf )    
   end
 end
