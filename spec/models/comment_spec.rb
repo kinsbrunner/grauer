b8e0c24fb8f0561e6f33d9b5443b3d48cceda01e
kinsbrunner <kinsbrunner@hotmail.com>
2016-08-03 11:53:51 -0400

    Added model tests for Comments and Evolutions

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  
+  # Valid factory test is on factories_spec.rb
+
+  it { should belong_to(:family) }  
+  it { should belong_to(:user) }  
 end
--- spec/models/evolution_spec.rb
+++ spec/models/evolution_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Evolution, type: :model do
-  
+  # Valid factory test is on factories_spec.rb
+
+  it { should validate_presence_of(:school_id) }
+  it { should validate_presence_of(:user_id) }
+  it { should belong_to(:school) }
+  it { should belong_to(:user) }  
 end
